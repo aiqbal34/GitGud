422476a33ec2793f14b88cf7bf2c47b6f7c4fe98
Aariz Iqbal <aariz.iqbal@outlook.com>
2024-02-21 23:53:42 -0800

    Initial Commit

---
+++ GitGud/GitGudApp.swift
@@ -0,0 +1,32 @@
+//
+//  GitGudApp.swift
+//  GitGud
+//
+//  Created by Aariz Iqbal on 2/21/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GitGudApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
