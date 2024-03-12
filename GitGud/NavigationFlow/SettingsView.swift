0f0e7d69b218e319f499206f45bc02b9a79cbaca
aiqbal34 <aariz.iqbal@outlook.com>
2024-03-12 12:26:29 -0700

    made a small change

--- GitGud/NavigationFlow/SettingsView.swift
+++ GitGud/NavigationFlow/SettingsView.swift
@@ -7,7 +7,7 @@ struct SettingsView: View {
     @State private var selectedTags: Set<String> = []
     @State private var searchText: String = ""
     @State var experience = ""
-    var experienceLevels = ["low", "medium", "high"]
+    var experienceLevels = ["beginner", "intermediate", "advanced"]
     @State var userName = ""
     @State var password = ""
     @State var email = ""
@@ -15,85 +15,106 @@ struct SettingsView: View {
     @State var major = ""
     @State var showSkillSheet = false
     @State var chosenSkills: [String] = []
+    @State var moveToLogin = false
 
     var body: some View {
         NavigationStack {
-            List {
-                
-                Section(header: Text("Account Information")){
-                    TextField(userModel.name, text: $userName)
-                    TextField(userModel.email, text: $email)
-                    TextField(userModel.university, text: $university)
-                    TextField(userModel.major, text: $major)
-                }
-                .listRowBackground(Color.secondaryBackground)
-                .foregroundColor(Color.text)
-                
-                Section(header: Text("Experience Level")){
-                    Picker("Select Major", selection: $experience) {
-                        ForEach(experienceLevels, id: \.self) { item in
-                            Text(item)
-                                .foregroundColor(.blue) // Change the text color
-                                .font(.headline) // Set the font style
-                                .padding() // Add some padding around each text
+            ZStack{
+                Color.background
+                    .edgesIgnoringSafeArea(.all)
+                VStack{
+                    Spacer()
+                    List {
+                        Section(header: Text("Account Information")){
+                            TextField(userModel.name, text: $userName)
+                            TextField(userModel.email, text: $email)
+                            TextField(userModel.university, text: $university)
+                            TextField(userModel.major, text: $major)
                         }
-                    }
-                    .pickerStyle(SegmentedPickerStyle()) // Apply MenuPickerStyle
-                    .padding() // Add padding around the picker
-                }
-                .onAppear {
-                    experience = userModel.experience
-                }
-                .listRowBackground(Color.secondaryBackground)
-                .foregroundColor(Color.text)
-                
-                HStack {
-                    Spacer()
-                    Button("Choose Skills") {
-                        showSkillSheet.toggle()
-                    }.sheet(isPresented: $showSkillSheet, content: {
-                        SearchViewModel(selectedItems: $chosenSkills, allItems: allSkills, itemLabel: { skill in
-                            Text(skill)
-                        }, filterPredicate: { skill, searchText in
-                            skill.lowercased().contains(searchText.lowercased())
-                        })
-                    })
-                    .frame(width: 200, height: 50)
-                    .background(Color.secondaryBackground)
-                    .foregroundColor(.text)
-                    .fontDesign(.monospaced)
-                    .cornerRadius(10)
-                    .fontWeight(.bold)
-                    .padding(.bottom)
-                    Spacer()
-                }
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 10) {
-                    ForEach(chosenSkills, id: \.self) { skill in
+                        .listRowBackground(Color.secondaryBackground)
+                        .foregroundColor(Color.text)
+                        
+                        Section(header: Text("Experience Level")){
+                            Picker("Select Major", selection: $experience) {
+                                ForEach(experienceLevels, id: \.self) { item in
+                                    Text(item)
+                                        .foregroundColor(.blue) // Change the text color
+                                        .font(.headline) // Set the font style
+                                        .padding() // Add some padding around each text
+                                }
+                            }
+                            .pickerStyle(SegmentedPickerStyle()) // Apply MenuPickerStyle
+                            .padding() // Add padding around the picker
+                        }
+                        .onAppear {
+                            experience = userModel.experience
+                        }
+                        .listRowBackground(Color.secondaryBackground)
+                        .foregroundColor(Color.text)
+                        
                         HStack {
-                            Text(skill)
-                                .padding()
-                                .background(Color.secondaryBackground)
-                                .foregroundColor(.text)
-                                .fontDesign(.monospaced)
-                                .clipShape(Capsule())
-                            
-                            Button(action: {
-                            }) {
-                                Image(systemName: "x.circle")
-                                    .foregroundColor(.red)
+                            Spacer()
+                            Button("Choose Skills") {
+                                showSkillSheet.toggle()
+                            }.sheet(isPresented: $showSkillSheet, content: {
+                                SearchViewModel(selectedItems: $chosenSkills, allItems: allSkills, itemLabel: { skill in
+                                    Text(skill)
+                                }, filterPredicate: { skill, searchText in
+                                    skill.lowercased().contains(searchText.lowercased())
+                                })
+                            })
+                            .frame(width: 200, height: 50)
+                            .background(Color.secondaryBackground)
+                            .foregroundColor(.text)
+                            .fontDesign(.monospaced)
+                            .cornerRadius(10)
+                            .fontWeight(.bold)
+                            .padding(.bottom)
+                            Spacer()
+                        }
+                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 10) {
+                            ForEach(chosenSkills, id: \.self) { skill in
+                                HStack {
+                                    Text(skill)
+                                        .padding()
+                                        .background(Color.secondaryBackground)
+                                        .foregroundColor(.text)
+                                        .fontDesign(.monospaced)
+                                        .clipShape(Capsule())
+                                    
+                                    Button(action: {
+                                    }) {
+                                        Image(systemName: "x.circle")
+                                            .foregroundColor(.red)
+                                    }
+                                }
                             }
                         }
+                        .padding()
                     }
+                    .listStyle(InsetGroupedListStyle())
+                    //.searchable(text: $searchText)
+                    
+                    .onAppear {
+                        chosenSkills = userModel.techStack
+                    }
+                    //Spacer()
+                    
+                    Button("Logout") {
+                        //TODO
+                        moveToLogin = true
+                    }
+                    Spacer()
                 }
-                .padding()
-            }
-            .listStyle(InsetGroupedListStyle())
-            .searchable(text: $searchText)
-            .navigationTitle("Settings")
-            .onAppear {
-                chosenSkills = userModel.techStack
             }
         }
+        .navigationTitle("Settings")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(isPresented: $moveToLogin){
+            LoginView()
+        }
+        
+        
     }
 }
 
