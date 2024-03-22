85a92f751cd4cace578fd010add6ea85fe0f8cb1
Hackingsimulator <smhussein@ucdavis.edu>
2024-03-22 15:01:58 -0700

    added comments

--- GitGud/TeamBuildFlow/AILoadingView.swift
+++ GitGud/TeamBuildFlow/AILoadingView.swift
@@ -7,18 +7,26 @@
 
 import SwiftUI
 
+/*
+ - Generates skills based on description
+ - On toggle, I'm feeling lucky, automates matching making
+ - Fetches users and sorts them based on skills
+ */
+
 struct AILoadingView: View {
     
+    // @Toggle to automate match making
     var imFeelingLucky: Bool
     
+    // @Objects
     var projectBuild: ProjectBuild
-    
     @State var pickMembersView = false
     @EnvironmentObject var userModel: UserModel
     @EnvironmentObject var UserTeams: UserTeamData
     @State var aiResponse: [AiResponse] = []
     @State var foundMembers: FoundMembers = FoundMembers(foundMembers: [])
     @State var isError: Bool = false
+    
     var body: some View {
         NavigationStack {
             ZStack {
@@ -31,39 +39,43 @@ struct AILoadingView: View {
             }
             .onAppear{
                 print(imFeelingLucky)
+                // On toggle, automates match making view
                 if imFeelingLucky {
                     Task {
                         aiResponse = try await sendReqToAiModel(description: projectBuild, urlString: "generateTeam")
                         for response in aiResponse {
+                            // Fetches users and sorts them based on skills
                             var filteredList = try await fetchFilteredList(skills: response.skills, experienceLevel: response.experienceLevel)
+                            // Assigns each members to the best fit
                             foundMembers.foundMembers.append(filteredList[0])
                         }
                         
                         pickMembersView = true
                     }
                 }else {
+                    // Manual match making
                     Task {
                         do {
+                            
                             aiResponse = try await sendReqToAiModel(description: projectBuild, urlString: "generateTeam")
-
+                            // Creates an array of sorted users to pick from
                             foundMembers.foundMembers = Array(repeating: nil, count: aiResponse.count)
                             pickMembersView = true
                         } catch {
                             print(error)
-                            
                             isError = true
-                            
                         }
                     }
                 }
             }
             .navigationDestination(isPresented: $pickMembersView){
-                FindMembersView(teamDescription: Team(people: [userModel.name], teamID: projectBuild.projectName + String(generateRandomNumber()), emails: [userModel.email], project: projectBuild), aiResponse: aiResponse, imFeelingLucky: imFeelingLucky)
-                    .environmentObject(userModel)
-                    .environmentObject(foundMembers)
-                    .environmentObject(UserTeams)
-              
-            }
+                // Navigates to FindMembersView
+                FindMembersView(aiResponse: aiResponse, teamDescription: Team(people: [userModel.name], teamID: projectBuild.projectName + String(generateRandomNumber()), emails: [userModel.email], project: projectBuild), imFeelingLucky: imFeelingLucky)
+                                .environmentObject(userModel)
+                                .environmentObject(foundMembers)
+                                .environmentObject(UserTeams)
+                          
+                        }
             .navigationBarBackButtonHidden()
             .alert(isPresented: $isError) {
                 Alert(title: Text("Welp"), message: Text("Something Went Wrong"), dismissButton: .default(Text("Cancel")))
